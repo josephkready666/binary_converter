57f930e730853d11e7f56f358238c79f9cb2d57d
josephkready666 <52659318+josephkready666@users.noreply.github.com>
2019-09-10 17:03:23 -0500

    Add files via upload
    Bonus is working. Still waiting on everyone else implementation.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ Members:
 #include<numeric>
 #include<algorithm>
 #include<string>
+#include<stdio.h> 
 #include<vector>
 using namespace std;
 
@@ -20,6 +21,12 @@ using namespace std;
 void main_menu(int*, int*, string* );
 int get_number_system();
 string get_value_base(int );
+int val(char c);
+int toDeci(char*, int);
+char reVal(int);
+void strev(char*);
+char* fromDeci(char[], int, int);
+
 
 
 int main() {
@@ -55,13 +62,19 @@ int main() {
 			break;
 		}
 
+		//BONUS
+		char* charValueInBase = &valueInBase[0];
+		solution = to_string(toDeci(charValueInBase, baseNumberSystem));
+		if(targetNumberSystem != 10){
+			char res[100];
+			solution = fromDeci(res, targetNumberSystem, stoi(solution));
+		}		
+		//BONUS
+
 		//Conversion Answer
 		cout << "\nAnswer: " << valueInBase << " in base " << baseNumberSystem << " is equal to " << solution << " in base " << targetNumberSystem << endl;
 
-
     }
-
-
 
     return (0);
 }
@@ -115,10 +128,6 @@ int get_number_system() {
 	return get_number_system();
 }
 
-	
-
-
-
 string get_value_base(int baseNumberSystem) {  
 	//Checks if inputed range is within 1-F
         
@@ -167,5 +176,89 @@ string get_value_base(int baseNumberSystem) {
     //Passes all input checks. 
     return valueBase;
     
+}
+
+
+
+int val(char c){
+	//Source: https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
+	// To return value of a char. For example, 2 is returned for '2'.  10 is returned for 'A', 11 for 'B' 
+	if (c >= '0' && c <= '9')
+		return (int)c - '0';
+	else
+		return (int)c - 'A' + 10;
+}
+
+ 
+int toDeci(char* str, int base){
+	//Source: https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
+	// Function to convert a number from given base 'b' to decimal
+	int len = strlen(str);
+	int power = 1; // Initialize power of base 
+	int num = 0;  // Initialize result 
+	int i;
+
+	// Decimal equivalent is str[len-1]*1 + 
+	// str[len-1]*base + str[len-1]*(base^2) + ... 
+	for (i = len - 1; i >= 0; i--)
+	{
+		// A digit in input number must be 
+		// less than number's base 
+		if (val(str[i]) >= base)
+		{
+			printf("Invalid Number");
+			return -1;
+		}
+
+		num += val(str[i]) * power;
+		power = power * base;
+	}
+
+	return num;
+}
+
+
+char reVal(int num){
+	//Source: https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
+	// To return char for a value. For example '2' is returned for 2. 'A' is returned for 10. 'B' for 11 
+	if (num >= 0 && num <= 9)
+		return (char)(num + '0');
+	else
+		return (char)(num - 10 + 'A');
+}
+
+
+void strev(char* str){
+	//Source: https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
+	// Utility function to reverse a string 
+	int len = strlen(str);
+	int i;
+	for (i = 0; i < len / 2; i++)
+	{
+		char temp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = temp;
+	}
+}
+
+
+char* fromDeci(char res[], int base, int inputNum){
+	//Source: https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
+	// Function to convert a given decimal number  to a base 'base' and 
+	int index = 0;  // Initialize index of result 
+
+		// Convert input number is given base by repeatedly 
+		// dividing it by base and taking remainder 
+	while (inputNum > 0)
+	{
+		res[index++] = reVal(inputNum % base);
+		inputNum /= base;
+	}
+	res[index] = '\0';
+
+	// Reverse the result 
+	strev(res);
+
+	return res;
 }
 
